f0057b1f6f3ce89ef20afa231aab11b5d7b950ab
André <alfr2@cin.ufpe.br>
2022-07-30 23:29:28 -0300

    atualizando step definitions com uns comentarios para meu grupo

--- test-acceptance/features/step_definitions/stepdefs.js
+++ test-acceptance/features/step_definitions/stepdefs.js
@@ -1,14 +1,17 @@
 const assert = require("assert");
 const { Given, When, Then } = require("cucumber");
 
-Given(" ", function() {
+Given("O usuário {string} não possui uma lista criada", function(usuario) {
+    //criar esse ambiente descrito no Given
     return "pending";
 });
 
-When(" ", function() {
-    return "pending";
+When("O usuário {string} vai para a página {string}", function(usuario, path) {
+    //realizar ação descrita no When
+    throw "nada criado ainda";
 });
 
-Then(" ", function() {
-    return "pending";
+Then("O site imprime a mensagem {string}", function(mensagem) {
+    //checar se os resultados do Then estão corretos
+    throw "nada criado ainda";
 });
