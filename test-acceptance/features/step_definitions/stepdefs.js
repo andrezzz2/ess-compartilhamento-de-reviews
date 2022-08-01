ee7249ebafe21b8e169da2cc4aa1a29a118cc223
André <alfr2@cin.ufpe.br>
2022-08-01 17:32:36 -0300

    reformulando e adicionando novos cenários e steps

--- test-acceptance/features/step_definitions/stepdefs.js
+++ test-acceptance/features/step_definitions/stepdefs.js
@@ -1,17 +1,53 @@
 const assert = require("assert");
 const { Given, When, Then } = require("cucumber");
 
-Given("O usuário {string} não possui uma lista criada", function(usuario) {
+
+Given("Eu estou logado com o usuário {string}", function(user){
     //criar esse ambiente descrito no Given
     return "pending";
 });
 
-When("O usuário {string} vai para a página {string}", function(usuario, path) {
+Given("Eu estou na página {string}", function(path){
+    //criar esse ambiente descrito no Given
+    return "pending";
+});
+
+When("Eu vou para a página {string}", function(user, path) {
     //realizar ação descrita no When
     throw "nada criado ainda";
 });
 
-Then("O site imprime a mensagem {string}", function(mensagem) {
+When("Eu clico para criar uma lista", function() {
+    //realizar ação descrita no When
+    throw "nada criado ainda";
+});
+
+When("Eu insiro o nome da lista {string}", function(listName) {
+    //realizar ação descrita no When
+    throw "nada criado ainda";
+});
+
+When("Eu clico para finalizar a criação da lista", function() {
+    //realizar ação descrita no When
+    throw "nada criado ainda";
+});
+
+When("Eu clico na lista {string}", function(listName) {
+    //realizar ação descrita no When
+    throw "nada criado ainda";
+});
+
+Then("O site mostra na tela a mensagem {string}", function(serverMsg) {
+    //checar se os resultados do Then estão corretos
+    throw "nada criado ainda";
+});
+
+Then("O site mostra uma tela para criação lista", function() {
+    //checar se os resultados do Then estão corretos
+    throw "nada criado ainda";
+});
+
+Then("O site mostra a lista {string}", function(listName) {
     //checar se os resultados do Then estão corretos
     throw "nada criado ainda";
 });
