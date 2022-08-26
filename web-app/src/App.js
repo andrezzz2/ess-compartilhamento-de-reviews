cabe3cbcf2883c155d60c53db7b152ac1c341a34
André <alfr2@cin.ufpe.br>
2022-08-25 21:49:27 -0300

    pagina do profile com box shadows, responsividade para telas verticais na pagina do profile e componente de listas quase pronto

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -9,7 +9,7 @@ function App() {
 
     //mock user
     const user = {
-        uid: 123,
+        id: "123",
         firstName: "André",
         lastName: "Luiz",
         username: "@andrezzz",
@@ -19,16 +19,116 @@ function App() {
         photoURL: "https://lh3.googleusercontent.com/a/AItbvmlht4uK2tnQF_wtuIBUKQzUS8_ZzOGCtdeXYK8H=s83-c-mo",
         followers: "10k",
         following: "293",
-        lists: {
-            movies:[],
-            series:[],
-            books:[]
-        },
-        reviews: {
-            movies:[],
-            series:[],
-            books:[]
-        },
+        moviesList:[{
+                    type: "movie",
+                    id: "1",
+                    title: "Thor: Amor e Trovão",
+                    photoURL: "https://lumiere-a.akamaihd.net/v1/images/56015l10_bigsal_brazil_intpayoff_4x5_d4019af6.jpeg",
+                    description: "“Thor: Amor e Trovão”, da Marvel Studios, encontra o Deus do Trovão numa jornada diferente de tudo o que já enfrentou – a procura pela paz interior. Mas a reforma de Thor é interrompida por um assassino galáctico conhecido como Gorr, o Carniceiro dos Deuses, que procura a extinção dos deuses. Para combater a ameaça, Thor pede a ajuda da Rei Valkiria, de Korg e da ex-namorada Jane Foster, que – para surpresa de Thor – empunha inexplicavelmente o seu martelo mágico, Mjolnir, e se intitula a Poderosa Thor. Juntos, eles embarcam numa angustiante aventura cósmica para descobrir o mistério da vingança do Carniceiro dos Deuses e detê-lo antes que seja tarde demais.",
+                    director: "Taika Waitili",
+                    reviewRate: "3",
+                    reviewId: "1",
+                    status: "watched"
+
+                },{
+                    type: "movie",
+                    id: "2",
+                    title: "Thor: Amor e Trovão",
+                    photoURL: "https://lumiere-a.akamaihd.net/v1/images/56015l10_bigsal_brazil_intpayoff_4x5_d4019af6.jpeg",
+                    description: "“Thor: Amor e Trovão”, da Marvel Studios, encontra o Deus do Trovão numa jornada diferente de tudo o que já enfrentou – a procura pela paz interior. Mas a reforma de Thor é interrompida por um assassino galáctico conhecido como Gorr, o Carniceiro dos Deuses, que procura a extinção dos deuses. Para combater a ameaça, Thor pede a ajuda da Rei Valkiria, de Korg e da ex-namorada Jane Foster, que – para surpresa de Thor – empunha inexplicavelmente o seu martelo mágico, Mjolnir, e se intitula a Poderosa Thor. Juntos, eles embarcam numa angustiante aventura cósmica para descobrir o mistério da vingança do Carniceiro dos Deuses e detê-lo antes que seja tarde demais.",
+                    director: "Taika Waitili",
+                    reviewRate: "",
+                    reviewId: "",
+                    status: "watching"
+                },{
+                    type: "movie",
+                    id: "3",
+                    title: "Thor: Amor e Trovão",
+                    photoURL: "https://lumiere-a.akamaihd.net/v1/images/56015l10_bigsal_brazil_intpayoff_4x5_d4019af6.jpeg",
+                    description: "“Thor: Amor e Trovão”, da Marvel Studios, encontra o Deus do Trovão numa jornada diferente de tudo o que já enfrentou – a procura pela paz interior. Mas a reforma de Thor é interrompida por um assassino galáctico conhecido como Gorr, o Carniceiro dos Deuses, que procura a extinção dos deuses. Para combater a ameaça, Thor pede a ajuda da Rei Valkiria, de Korg e da ex-namorada Jane Foster, que – para surpresa de Thor – empunha inexplicavelmente o seu martelo mágico, Mjolnir, e se intitula a Poderosa Thor. Juntos, eles embarcam numa angustiante aventura cósmica para descobrir o mistério da vingança do Carniceiro dos Deuses e detê-lo antes que seja tarde demais.",
+                    director: "Taika Waitili",
+                    reviewRate: "",
+                    reviewId: "",
+                    status: "abandoned"
+                }
+            ],
+        seriesList:[{
+                    type: "serie",
+                    id: "4",
+                    title: "Stranger Things",
+                    season: "3",
+                    photoURL: "https://br.web.img2.acsta.net/pictures/19/07/10/20/01/2331295.jpg",
+                    description: "A terceira temporada de Stranger Things foi anunciada pela Netflix em dezembro de 2017. Matt Duffer e Ross Duffer continuam como showrunners e produtores executivos. A terceira temporada estreou em 4 de julho de 2019, com 8 episódios, estrelada por Winona Ryder, David Harbour, Finn Wolfhard, Millie Bobby Brown, Gaten Matarazzo, Caleb McLaughlin, Noah Schnapp, Sadie Sink, Natalia Dyer, Charlie Heaton, Joe Keery, Dacre Montgomery, Maya Hawke, Priah Ferguson e Cara Buono, com Brett Gelman, Cary Elwes, Jake Busey, Michael Park, Francesca Reale e Alec Utgoff em papéis recorrentes. As atuações de Harbour, Brown, Montgomery e Hawke foram elogiadas pela crítica.",
+                    reviewRate: "3",
+                    reviewId: "2",
+                    status: "watched"
+
+                },{ 
+                    type: "serie",
+                    id: "5",
+                    title: "Stranger Things",
+                    season: "3",
+                    photoURL: "https://br.web.img2.acsta.net/pictures/19/07/10/20/01/2331295.jpg",
+                    description: "A terceira temporada de Stranger Things foi anunciada pela Netflix em dezembro de 2017. Matt Duffer e Ross Duffer continuam como showrunners e produtores executivos. A terceira temporada estreou em 4 de julho de 2019, com 8 episódios, estrelada por Winona Ryder, David Harbour, Finn Wolfhard, Millie Bobby Brown, Gaten Matarazzo, Caleb McLaughlin, Noah Schnapp, Sadie Sink, Natalia Dyer, Charlie Heaton, Joe Keery, Dacre Montgomery, Maya Hawke, Priah Ferguson e Cara Buono, com Brett Gelman, Cary Elwes, Jake Busey, Michael Park, Francesca Reale e Alec Utgoff em papéis recorrentes. As atuações de Harbour, Brown, Montgomery e Hawke foram elogiadas pela crítica.",
+                    reviewRate: "",
+                    reviewId: "",
+                    status: "watching"
+                },{
+                    type: "serie",
+                    id: "6",
+                    title: "Stranger Things",
+                    season: "3",
+                    photoURL: "https://br.web.img2.acsta.net/pictures/19/07/10/20/01/2331295.jpg",
+                    description: "A terceira temporada de Stranger Things foi anunciada pela Netflix em dezembro de 2017. Matt Duffer e Ross Duffer continuam como showrunners e produtores executivos. A terceira temporada estreou em 4 de julho de 2019, com 8 episódios, estrelada por Winona Ryder, David Harbour, Finn Wolfhard, Millie Bobby Brown, Gaten Matarazzo, Caleb McLaughlin, Noah Schnapp, Sadie Sink, Natalia Dyer, Charlie Heaton, Joe Keery, Dacre Montgomery, Maya Hawke, Priah Ferguson e Cara Buono, com Brett Gelman, Cary Elwes, Jake Busey, Michael Park, Francesca Reale e Alec Utgoff em papéis recorrentes. As atuações de Harbour, Brown, Montgomery e Hawke foram elogiadas pela crítica.",
+                    reviewRate: "5",
+                    reviewId: "4",
+                    status: "abandoned"
+                }
+        ],
+        booksList:[{
+                    type: "book",
+                    id: "7",
+                    title: "Missão Romance",
+                    photoURL: "https://images-na.ssl-images-amazon.com/images/I/81RobDsEGnL.jpg",
+                    description: "Segundo volume da série Clube do livro dos homens.Missão Romance foi considerado um dos melhores romances de 2020 pelo site Oprahdaily“Lyssa entremeia romance, suspense e humor de forma habilidosa, em uma trama com personagens fortes e uma química irresistível entre os protagonistas.” – Publishers Weekly Liv Papandreas tem o emprego dos sonhos: ela é chef confeiteira do restaurante mais badalado de Nashville, comandado por Royce Preston, uma celebridade da TV. O problema é que, longe das câmeras, seu chefe é um mau-caráter.Certo dia, Liv flagra Royce assediando uma jovem recepcionista e, ao confrontá-lo, acaba demitida. Ela jura vingança, mas vai precisar de ajuda para derrotar alguém tão poderoso.Infelizmente, isso significa recorrer a Braden Mack, um empresário carismático e mulherengo. Ele se oferece para revelar ao mundo quem Royce realmente é, mas Liv tem dificuldade em acreditar nas suas boas intenções. Assim, Mack precisa chamar reforços: os integrantes do Clube do Livro dos Homens.Inspirados pelo romance que estão lendo, o Clube do Livro se une a Liv para derrubar Royce. Paralelamente, eles tentam ajudar Mack a encontrar o caminho para o coração de Liv... mesmo que ela esteja determinada a ignorar qualquer chama de paixão que surgir entre os dois.",
+                    author: "Lyss Kay Adams",
+                    reviewRate: "0",
+                    reviewId: "5",
+                    status: "read",
+                },{
+                    type: "book",
+                    id: "8",
+                    title: "Missão Romance",
+                    photoURL: "https://images-na.ssl-images-amazon.com/images/I/81RobDsEGnL.jpg",
+                    description: "Segundo volume da série Clube do livro dos homens.Missão Romance foi considerado um dos melhores romances de 2020 pelo site Oprahdaily“Lyssa entremeia romance, suspense e humor de forma habilidosa, em uma trama com personagens fortes e uma química irresistível entre os protagonistas.” – Publishers Weekly Liv Papandreas tem o emprego dos sonhos: ela é chef confeiteira do restaurante mais badalado de Nashville, comandado por Royce Preston, uma celebridade da TV. O problema é que, longe das câmeras, seu chefe é um mau-caráter.Certo dia, Liv flagra Royce assediando uma jovem recepcionista e, ao confrontá-lo, acaba demitida. Ela jura vingança, mas vai precisar de ajuda para derrotar alguém tão poderoso.Infelizmente, isso significa recorrer a Braden Mack, um empresário carismático e mulherengo. Ele se oferece para revelar ao mundo quem Royce realmente é, mas Liv tem dificuldade em acreditar nas suas boas intenções. Assim, Mack precisa chamar reforços: os integrantes do Clube do Livro dos Homens.Inspirados pelo romance que estão lendo, o Clube do Livro se une a Liv para derrubar Royce. Paralelamente, eles tentam ajudar Mack a encontrar o caminho para o coração de Liv... mesmo que ela esteja determinada a ignorar qualquer chama de paixão que surgir entre os dois.",
+                    author: "Lyss Kay Adams",
+                    reviewRate: "",
+                    reviewId: "",
+                    status: "reading",
+                },{
+                    type: "book",
+                    id: "9",
+                    title: "Missão Romance",
+                    photoURL: "https://images-na.ssl-images-amazon.com/images/I/81RobDsEGnL.jpg",
+                    description: "Segundo volume da série Clube do livro dos homens.Missão Romance foi considerado um dos melhores romances de 2020 pelo site Oprahdaily“Lyssa entremeia romance, suspense e humor de forma habilidosa, em uma trama com personagens fortes e uma química irresistível entre os protagonistas.” – Publishers Weekly Liv Papandreas tem o emprego dos sonhos: ela é chef confeiteira do restaurante mais badalado de Nashville, comandado por Royce Preston, uma celebridade da TV. O problema é que, longe das câmeras, seu chefe é um mau-caráter.Certo dia, Liv flagra Royce assediando uma jovem recepcionista e, ao confrontá-lo, acaba demitida. Ela jura vingança, mas vai precisar de ajuda para derrotar alguém tão poderoso.Infelizmente, isso significa recorrer a Braden Mack, um empresário carismático e mulherengo. Ele se oferece para revelar ao mundo quem Royce realmente é, mas Liv tem dificuldade em acreditar nas suas boas intenções. Assim, Mack precisa chamar reforços: os integrantes do Clube do Livro dos Homens.Inspirados pelo romance que estão lendo, o Clube do Livro se une a Liv para derrubar Royce. Paralelamente, eles tentam ajudar Mack a encontrar o caminho para o coração de Liv... mesmo que ela esteja determinada a ignorar qualquer chama de paixão que surgir entre os dois.",
+                    author: "Lyss Kay Adams",
+                    reviewRate: "",
+                    reviewId: "",
+                    status: "abandoned",
+                }
+        ],
+        reviews: [{
+                    id: "5",
+                    ownerId: "123",
+                    type: "book",
+                    title: "Pior livro",
+                    bookId: "7",
+                    bookTitle: "Missão Romance",
+                    rate: "0",
+                    text: "uma merda, blablablablablablablablablabalablaalbala",
+                    date: "25-08-2022"
+                }
+        ],
         followersList: [],
         followingList: []
     };
--- web-app/src/Components/Lists/index.js
+++ web-app/src/Components/Lists/index.js
@@ -4,6 +4,133 @@ function Lists ( {User} ){
 
     return (
         <div className="Lists">
+            
+            <div className='ListContainer'>
+
+                <div className='ListOptions'>
+
+                    <span className='ListTitle'> Movies </span>
+
+                    <div className='AddToList'>
+                        <img alt='add to list icon' src=''></img>
+                    </div>
+
+                </div>
+
+                <div className='List'>
+
+                    <article className='Item'>
+                        <img alt='' src='https://lumiere-a.akamaihd.net/v1/images/56015l10_bigsal_brazil_intpayoff_4x5_d4019af6.jpeg'></img>
+                        <span className='ItemTitle'>Filme 1</span>
+                        <span className='ItemStatus'>assistido</span>
+                    </article>
+
+                    <article className='Item'>
+                        <img alt='' src='https://lumiere-a.akamaihd.net/v1/images/56015l10_bigsal_brazil_intpayoff_4x5_d4019af6.jpeg'></img>
+                        <span className='ItemTitle'>Filme 2</span>
+                        <span className='ItemStatus'>assistido</span>
+                    </article>
+
+                    <article className='Item'>
+                        <img alt='' src='https://lumiere-a.akamaihd.net/v1/images/56015l10_bigsal_brazil_intpayoff_4x5_d4019af6.jpeg'></img>
+                        <span className='ItemTitle'>Filme 3</span>
+                        <span className='ItemStatus'>assistido</span>
+                    </article>
+
+                    <article className='Item'>
+                        <img alt='' src='https://lumiere-a.akamaihd.net/v1/images/56015l10_bigsal_brazil_intpayoff_4x5_d4019af6.jpeg'></img>
+                        <span className='ItemTitle'>Filme 4</span>
+                        <span className='ItemStatus'>assistido</span>
+                    </article>
+
+                </div>
+
+            </div>
+            
+
+            <div className='ListContainer'>
+
+                <div className='ListOptions'>
+
+                    <span className='ListTitle'> Series </span>
+
+                    <div className='AddToList'>
+                        <img alt='add to list icon'></img>
+                    </div>
+
+                </div>
+
+                <div className='List'>
+
+                    <article className='Item'>
+                        <img alt='' src='https://lumiere-a.akamaihd.net/v1/images/56015l10_bigsal_brazil_intpayoff_4x5_d4019af6.jpeg'></img>
+                        <span className='ItemTitle'>Serie 1</span>
+                        <span className='ItemStatus'>assistido</span>
+                    </article>
+
+                    <article className='Item'>
+                        <img alt='' src='https://lumiere-a.akamaihd.net/v1/images/56015l10_bigsal_brazil_intpayoff_4x5_d4019af6.jpeg'></img>
+                        <span className='ItemTitle'>Serie 2</span>
+                        <span className='ItemStatus'>assistido</span>
+                    </article>
+
+                    <article className='Item'>
+                        <img alt='' src='https://lumiere-a.akamaihd.net/v1/images/56015l10_bigsal_brazil_intpayoff_4x5_d4019af6.jpeg'></img>
+                        <span className='ItemTitle'>Serie 3</span>
+                        <span className='ItemStatus'>assistido</span>
+                    </article>
+
+                    <article className='Item'>
+                        <img alt='' src='https://lumiere-a.akamaihd.net/v1/images/56015l10_bigsal_brazil_intpayoff_4x5_d4019af6.jpeg'></img>
+                        <span className='ItemTitle'>Serie 4</span>
+                        <span className='ItemStatus'>assistido</span>
+                    </article>
+
+                </div>
+
+            </div>
+
+            <div className='ListContainer'>
+
+                <div className='ListOptions'>
+
+                    <span className='ListTitle'> Books </span>
+
+                    <div className='AddToList'>
+                        <img alt='add to list icon'></img>
+                    </div>
+
+                </div>
+
+                <div className='List'>
+
+                    <article className='Item'>
+                        <img alt='' src='https://lumiere-a.akamaihd.net/v1/images/56015l10_bigsal_brazil_intpayoff_4x5_d4019af6.jpeg'></img>
+                        <span className='ItemTitle'>Livro 1</span>
+                        <span className='ItemStatus'>assistido</span>
+                    </article>
+
+                    <article className='Item'>
+                        <img alt='' src='https://lumiere-a.akamaihd.net/v1/images/56015l10_bigsal_brazil_intpayoff_4x5_d4019af6.jpeg'></img>
+                        <span className='ItemTitle'>Livro 2</span>
+                        <span className='ItemStatus'>assistido</span>
+                    </article>
+
+                    <article className='Item'>
+                        <img alt='' src='https://lumiere-a.akamaihd.net/v1/images/56015l10_bigsal_brazil_intpayoff_4x5_d4019af6.jpeg'></img>
+                        <span className='ItemTitle'>Livro 3</span>
+                        <span className='ItemStatus'>assistido</span>
+                    </article>
+
+                    <article className='Item'>
+                        <img alt='' src='https://lumiere-a.akamaihd.net/v1/images/56015l10_bigsal_brazil_intpayoff_4x5_d4019af6.jpeg'></img>
+                        <span className='ItemTitle'>Livro 4</span>
+                        <span className='ItemStatus'>assistido</span>
+                    </article>
+
+                </div>
+
+            </div>
 
         </div>
     )
