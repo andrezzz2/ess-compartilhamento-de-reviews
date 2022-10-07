85100f120dab0b6aa77d566440ef81474e9fdeb5
Alansidney <asj2@cin.ufpe.br>
2022-10-06 22:58:26 -0300

    Redesign do componente Followers 50%

--- web-app/src/Components/Followers/index.js
+++ web-app/src/Components/Followers/index.js
@@ -1,45 +1,39 @@
 import axios from 'axios';
 import './Styles.css';
-import {useState,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 
-function Followers ( {requestedUser, User} ){
+function Followers({requestedUser}) {
 
-    const [lista, updateLista] = useState([]);
-    const [tupla, updateTupla] = useState([]);
-    
-    useEffect (()=>{
-        if(lista.length === 0){
-            requestedUser.followersList.forEach((follower)=> {
-                axios.get('http://localhost:8080/user/getinfo/'+follower).then((response)=>{
-                    updateLista( arr => [...arr, response.data.user.photoURL]);
+    const [followersInfo, updateFollowersInfo] = useState([]);
+
+    useEffect(() => {
+        if(followersInfo.length == 0){
+            requestedUser.followersList.forEach((follower) => {
+                axios.get('http://localhost:8080/user/getinfo/' + follower).then((response) => {
+                    updateFollowersInfo(arr => [...arr, [follower, response.data.user.photoURL, (response.data.user.firstName + " " + response.data.user.lastName),response.data.user.bio]]);
                 });
             });
         }
-        console.log("lista de fotos:",lista);
-    },[]);
+        console.log("followersInfo de fotos:", followersInfo);
+    }, []);
 
-    useEffect (()=>{
-        if(lista.length === requestedUser.followersList.length){
-            for(let i = 0; i<lista.length;i+=1){
-                updateTupla( arr => [...arr, [requestedUser.followersList[i], lista[i]]]);
-            }
-        }
-        console.log("tupla de usuario",tupla);
-    },[lista]);
-    
+
+
     return (
         <div className="Followers">
             <div className="Followers-div">
-                {tupla?.map((follower) =>{
-                    return( 
-                        <div key={follower[0]} className="Followers-container">
-                            <a href={"http://localhost:3000/profile/"+follower[0]}>
-                                <img id='friend-icon' alt="Friend Icon" src={follower[1]}/>
-                            </a>
-                            
-                            <a href={"http://localhost:3000/profile/"+follower[0]}>
-                                <p id='friend-nick'>{follower[0]}</p>
+                {followersInfo?.map((follower) => {
+                    return (
+                        <div key={follower[0]} >
+                            <a className="Followers-container" href={"http://localhost:3000/profile/" + follower[0]}>
+                                <img id='friend-icon' alt="Friend Icon" src={follower[1]} />
+                                <div className="friend-info">
+                                    <p id ='friend-name'>{follower[2]}</p>
+                                    <p id='friend-nick'>@{follower[0]}</p>
+                                    <p id='friend-bio'>{follower[3]}</p>
+                                </div>
+                                
                             </a>
                         </div>
                     )
