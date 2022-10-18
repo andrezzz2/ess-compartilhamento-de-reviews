6fe5d5f4189feaabcddb10c70376d90303d2ca6e
romildojuliano <rjal@cin.ufpe.br>
2022-10-18 13:09:08 -0300

    fix(index.js, Styles.css): Front-end improved

--- web-app/src/Components/Reviews/index.js
+++ web-app/src/Components/Reviews/index.js
@@ -4,66 +4,78 @@ function Reviews ( {requestedUser} ){
     function ShowReviewByType () {
         var movieIndex, bookIndex, serieIndex
         var currentMovie, currentBook, currentSerie
-        return (
-            <div>
-            {
-            requestedUser.reviews.map(
-                review => {
-                    switch (review.type) {
-                        case "movie":
-                            movieIndex = parseInt(review.itemId)
-                            currentMovie = requestedUser.moviesList[movieIndex]
-                            return (
-                                <div className="ReviewContainer">
-                                    {currentMovie.title}
-                                    <div className="InlineContainer">
-                                        <img className="ReviewImg" src={currentMovie.imageURL} alt="filme"></img>
-                                        <div className='ReviewText'>
-                                            {review.text}
+        if (requestedUser.reviews.length <= 0) {
+            return (
+                <div>
+                    {
+                    requestedUser.reviews.map(
+                        review => {
+                            switch (review.type) {
+                                case "movie":
+                                    movieIndex = parseInt(review.itemId)
+                                    currentMovie = requestedUser.moviesList[movieIndex]
+                                    return (
+                                        <div className="ReviewContainer">
+                                            <div className="WhiteBoxContainer">
+                                                
+                                                <div className="InlineContainer">
+                                                    <div className="ReviewImg">
+                                                        <img src={currentMovie.imageURL} alt="filme"></img>
+                                                    </div>
+                                                    <div className="TitleName">
+                                                        {currentMovie.title}
+                                                    </div>
+                                                    <div className='ReviewText'>
+                                                            {review.text}
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                            );
-                        case "book":
-                            bookIndex = parseInt(review.itemId)
-                            currentBook = requestedUser.booksList[bookIndex]
-                            return (
-                                <div className="ReviewContainer">
-                                    {currentBook.title}
-                                    <div className="InlineContainer">
-                                        <img className="ReviewImg" src={currentBook.imageURL} alt="imagem"></img>
-                                        <div className='ReviewText'>
-                                            {review.text}
+                                    );
+                                case "book":
+                                    bookIndex = parseInt(review.itemId)
+                                    currentBook = requestedUser.booksList[bookIndex]
+                                    return (
+                                        <div className="ReviewContainer">
+                                            {currentBook.title}
+                                            <div className="WhiteBoxContainer"> 
+                                                <div className="InlineContainer">
+                                                    <img className="ReviewImg" src={currentBook.imageURL} alt="imagem"></img>
+                                                    <div className='ReviewText'>
+                                                        {review.text}
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                            );
-                        case "serie":
-                            serieIndex = parseInt(review.itemId)
-                            currentSerie = requestedUser.seriesList[serieIndex]
-                            return (
-                                <div className="ReviewContainer">
-                                    {currentSerie.title}
-                                    <div className="InlineContainer">
-                                        <img className="ReviewImg" src={currentSerie.imageURL} alt="serie"></img>
-                                        <div className='ReviewText'>
-                                            {review.text}
+                                    );
+                                case "serie":
+                                    serieIndex = parseInt(review.itemId)
+                                    currentSerie = requestedUser.seriesList[serieIndex]
+                                    return (
+                                        <div className="ReviewContainer">
+                                            {currentSerie.title}
+                                            <div className="InlineContainer">
+                                                <img className="ReviewImg" src={currentSerie.imageURL} alt="serie"></img>
+                                                <div className='ReviewText'>
+                                                    {review.text}
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                            );
-                        default:
-                            return (
-                                <div className="ReviewContainer">
-                                </div>
-                            );
-                    } 
+                                    );
+                                default:
+                                    return (
+                                        <div className="ReviewContainer">
+                                        </div>
+                                    );
+                            } 
+                        }
+                    )
                 }
+            </div>
             )
         }
-        </div>
-        )
-    }
+        }
+        
 
     return (
         <div className="Reviews">
